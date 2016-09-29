5192e102c2c4138609a959dd3dc91fd120173136
tinysh <438985635@qq.com>
2016-09-29 22:29:47 +0800

    更新

--- chiki/web/__init__.py
+++ chiki/web/__init__.py
@@ -2,16 +2,17 @@
 from flask import request, render_template
 
 
-def message(msg, style='info', url='', timeout=0):
-    return render_template('msg.html', msg=msg, style=style, url=url, timeout=timeout)
+def message(msg, style='info', url='', timeout=0, **kwargs):
+    return render_template('msg.html', msg=msg, style=style,
+                           url=url, timeout=timeout, **kwargs)
 
 
-def success(msg, url='', timeout=0):
-    return message(msg, style='success', url=url, timeout=timeout)
+def success(msg, url='', timeout=0, **kwargs):
+    return message(msg, style='success', url=url, timeout=timeout, **kwargs)
 
 
-def error(msg, url='', timeout=0):
-    return message(msg, style='warn', url=url, timeout=timeout)
+def error(msg, url='', timeout=0, **kwargs):
+    return message(msg, style='warn', url=url, timeout=timeout, **kwargs)
 
 
 def is_ajax():
