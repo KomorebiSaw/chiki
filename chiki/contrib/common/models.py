a9dcca3f1400de69c201d4d9439923123a4ce5fc
tinysh <438985635@qq.com>
2016-10-07 23:31:25 +0800

    修复bug

--- chiki/contrib/common/models.py
+++ chiki/contrib/common/models.py
@@ -498,6 +498,7 @@ class Action(db.Document):
     ios_start = db.ReferenceField(IOSVersion, verbose_name='IOS版本')
     ios_end = db.ReferenceField(IOSVersion, verbose_name='IOS最大版本')
     login = db.BooleanField(default=False, verbose_name='登陆')
+    debug = db.BooleanField(default=False, verbose_name='调试')
     enable = db.StringField(default=Enable.ENABLED, verbose_name='状态', choices=Enable.CHOICES)
     modified = db.DateTimeField(default=datetime.now, verbose_name='修改时间')
     created = db.DateTimeField(default=datetime.now, verbose_name='创建时间')
@@ -522,6 +523,7 @@ class Action(db.Document):
             target=self.target,
             share=unicode(self.share),
             login=self.login,
+            debug=self.debug,
         )
 
 
