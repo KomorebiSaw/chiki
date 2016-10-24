a94d1a4c373001094c800177cc2898289eb01566
tinysh <438985635@qq.com>
2016-10-24 20:49:39 +0800

    更新

--- chiki/oauth/wxmsg.py
+++ chiki/oauth/wxmsg.py
@@ -18,49 +18,50 @@ class WXMsg(object):
             self.init_app(app)
 
     def init_app(self, app):
+        self.init_robot(robot)
         robot.init_app(app)
         robot.logger = app.logger
+
+    def init_robot(self, robot):
+        @robot.text
+        def on_text(message):
+            msg = Message.objects(keyword=message.content).first()
+            if msg:
+                reply = msg.reply(message)
+                if reply:
+                    return reply
+
+            default_msg = Message.objects(default=True).first()
+            if default_msg:
+                return default_msg.reply(message)
+            return ''
+
+        @robot.subscribe
+        @robot.scan
+        def on_subscribe(message):
+            if self.subscribe_callback:
+                res = self.subscribe_callback(message)
+                if res:
+                    return res
+            follow_msg = Message.objects(follow=True).first()
+            if follow_msg:
+                return follow_msg.reply(message)
+            default_msg = Message.objects(default=True).first()
+            if default_msg:
+                return default_msg.reply(message)
+            return ''
+
+        @robot.unsubscribe
+        def on_unsubscribe(message):
+            user = um.models.User.get_wechat(mp_openid=message.source)
+            if user:
+                user.wechat_user.unsubscribe()
 
     def subscribe_handler(self, callback):
         self.subscribe_callback = callback
         return callback
 
-    @robot.text
-    @staticmethod
-    def on_text(message):
-        msg = Message.objects(keyword=message.content).first()
-        if msg:
-            reply = msg.reply(message)
-            if reply:
-                return reply
 
-        default_msg = Message.objects(default=True).first()
-        if default_msg:
-            return default_msg.reply(message)
-        return ''
-
-    @robot.subscribe
-    @robot.scan
-    @staticmethod
-    def on_subscribe(message):
-        if self.subscribe_callback:
-            res = self.subscribe_callback(message)
-            if res:
-                return res
-        follow_msg = Message.objects(follow=True).first()
-        if follow_msg:
-            return follow_msg.reply(message)
-        default_msg = Message.objects(default=True).first()
-        if default_msg:
-            return default_msg.reply(message)
-        return ''
-
-    @robot.unsubscribe
-    @staticmethod
-    def on_unsubscribe(message):
-        user = um.models.User.get_wechat(mp_openid=message.source)
-        if user:
-            user.wechat_user.unsubscribe()
 
 
 def init_wxmsg(app):
