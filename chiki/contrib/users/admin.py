02eb0a037eaaca47705d45ac3dff1ea3f3e7f962
loony <369864384@qq.com>
2016-11-07 15:23:27 +0800

    update-view

--- chiki/contrib/users/admin.py
+++ chiki/contrib/users/admin.py
@@ -29,11 +29,13 @@ class WeChatUserView(ModelView):
     show_popover = True
     column_default_sort = ('created', True)
     column_list = (
-        'user', 'scene', 'nickname', 'province', 'city', 'privilege', 'subscribe', 'subscribe_time', 'remark', 'groupid', 'access_token', 
+        'user', 'scene', 'nickname', 'province', 'city', 'privilege',
+        'subscribe', 'subscribe_time', 'remark', 'groupid', 'access_token',
         'expires_in', 'refresh_token', 'updated', 'modified', 'created'
     )
     column_center_list = (
-        'user', 'nickname', 'province', 'city', 'privilege', 'subscribe', 'subscribe_time', 'remark', 'groupid', 'access_token', 
+        'user', 'nickname', 'province', 'city', 'privilege', 'subscribe',
+        'subscribe_time', 'remark', 'groupid', 'access_token',
         'expires_in', 'refresh_token', 'updated', 'modified', 'created'
     )
     column_searchable_list = ('unionid', 'mp_openid', )
@@ -43,8 +45,14 @@ class WeChatUserView(ModelView):
 class QQUserView(ModelView):
     show_popover = True
     column_default_sort = ('created', True)
-    column_list = ('user', 'openid', 'is_yellow_vip', 'is_yellow_year_vip', 'access_token' ,'expires_in', 'refresh_token', 'modified', 'created')
-    column_center_list = ('user', 'openid', 'is_yellow_vip', 'is_yellow_year_vip', 'access_token' ,'expires_in', 'refresh_token', 'modified', 'created')
+    column_list = (
+        'user', 'openid', 'is_yellow_vip', 'is_yellow_year_vip',
+        'access_token', 'expires_in', 'refresh_token', 'modified', 'created'
+    )
+    column_center_list = (
+        'user', 'openid', 'is_yellow_vip', 'is_yellow_year_vip',
+        'access_token', 'expires_in', 'refresh_token', 'modified', 'created'
+    )
     column_searchable_list = ('openid',)
     column_filters = ('openid', 'is_yellow_vip', 'vip', 'yellow_vip_level', 'level', 'is_yellow_year_vip')
 
@@ -52,19 +60,25 @@ class QQUserView(ModelView):
 class WeiBoUserView(ModelView):
     show_popover = True
     column_default_sort = ('created', True)
-    column_list = ('user', 'uid', 'subscribe', 'subscribe_time', 'follow', 'access_token', 'expires_in', 'refresh_token', 'modified', 'created')
-    column_center_list = ('user', 'uid', 'subscribe', 'subscribe_time', 'follow', 'access_token', 'expires_in', 'refresh_token', 'modified', 'created')
+    column_list = (
+        'user', 'uid', 'subscribe', 'subscribe_time', 'follow',
+        'access_token', 'expires_in', 'refresh_token', 'modified', 'created'
+    )
+    column_center_list = (
+        'user', 'uid', 'subscribe', 'subscribe_time', 'follow', 'access_token',
+        'expires_in', 'refresh_token', 'modified', 'created'
+    )
     column_searchable_list = ('province', 'city',)
     column_filters = ('user', 'uid')
 
 
 class UserLogView(ModelView):
-    pass
+    column_center_list = ('user', 'type', 'key', 'device', 'spm', 'ip', 'created')
 
 
 class PhoneCodeView(ModelView):
-    pass
+    column_center_list = ('phone', 'action', 'code', 'error', 'created')
 
 
 class EmailCodeView(ModelView):
-    pass
+    column_center_list = ('email', 'action', 'code', 'token', 'error', 'created')
