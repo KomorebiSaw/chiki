ade1e21179c80d525f7fbec9a17461d6b54237d1
tinysh <438985635@qq.com>
2016-11-28 22:01:46 +0800

    更新

--- chiki/oauth/alipay.py
+++ chiki/oauth/alipay.py
@@ -27,8 +27,8 @@ class Alipay(object):
         self.endpoint = self.config.get('endpoint', 'alipay_callback')
         self.app_id = self.config.get('app_id')
         try:
-            self.app_private_key = rsa.PrivateKey.load_pkcs1(self.config.get('app_private_key'))
-            self.alipay_public_key = rsa.PublicKey.load_pkcs1(self.config.get('alipay_public_key'))
+            self.app_private_key = rsa.PrivateKey.load_pkcs1_openssl_pem(self.config.get('app_private_key'))
+            self.alipay_public_key = rsa.PublicKey.load_pkcs1_openssl_pem(self.config.get('alipay_public_key'))
         except ValueError:
             app.logger.error(traceback.format_exc())
             self.app_private_key = None
