2a573ea8172985edf1efda38ec8cb022024f0bab
tiger <438985635@qq.com>
2016-12-15 11:48:12 +0800

    update

--- chiki/oauth/wxmsg.py
+++ chiki/oauth/wxmsg.py
@@ -1,4 +1,5 @@
 # coding: utf-8
+import traceback
 from chiki.contrib.users import um
 from flask import current_app
 from werobot.reply import TransferCustomerServiceReply
@@ -28,62 +29,59 @@ class WXMsg(object):
     def init_robot(self, robot):
         @robot.text
         def on_text(message):
-            msg = Message.objects(keyword=message.content).first()
-            if msg:
-                reply = msg.reply(message)
-                if reply:
-                    return reply
+            try:
+                msg = Message.objects(keyword=message.content).first()
+                if msg:
+                    reply = msg.reply(message)
+                    if reply:
+                        return reply
 
-            current_app.logger.error('test5')
+                default_msg = Message.objects(default=True).first()
+                if default_msg:
+                    reply = default_msg.reply(message)
+                    if reply:
+                        return reply
 
-            default_msg = Message.objects(default=True).first()
-            if default_msg:
-                reply = default_msg.reply(message)
-                if reply:
-                    return reply
-
-            current_app.logger.error('test6')
-            return TransferCustomerServiceReply(message=message)
+                return TransferCustomerServiceReply(message=message)
+            except:
+                current_app.logger.error(traceback.format_ext())
+                return '系统繁忙，请稍后重试！'
 
         @robot.subscribe
         @robot.scan
         def on_subscribe(message):
-            user = um.models.User.get_wechat(mp_openid=message.source)
-            if not user:
-                user = um.models.User.from_wechat_mp(message.source)
+            try:
+                user = um.models.User.get_wechat(mp_openid=message.source)
+                if not user:
+                    user = um.models.User.from_wechat_mp(message.source)
 
-            current_app.logger.error('test1')
+                um.models.on_invite(user, int(message.key or 0))
 
-            um.models.on_invite(user, int(message.key or 0))
+                if self.subscribe_callback:
+                    res = self.subscribe_callback(user, message)
+                    if res:
+                        return res
 
-            if self.subscribe_callback:
-                res = self.subscribe_callback(user, message)
-                if res:
-                    return res
+                if current_app.config.get('NEED_QRCODE') and not user.inviter:
+                    return '%s，欢迎关注！请取消关注，再通过扫描海报二维码进行关注！' % user.nickname
 
-            current_app.logger.error('test2')
+                if not user.wechat_user.subscribe:
+                    user.wechat_user.dosubscribe()
 
-            if current_app.config.get('NEED_QRCODE') and not user.inviter:
-                return '%s，欢迎关注！请取消关注，再通过扫描海报二维码进行关注！' % user.nickname
-
-            if not user.wechat_user.subscribe:
-                user.wechat_user.dosubscribe()
-
-            current_app.logger.error('test3')
-
-            follow_msg = Message.objects(follow=True).first()
-            if follow_msg:
-                reply = follow_msg.reply(message)
-                if reply:
-                    return reply
-            default_msg = Message.objects(default=True).first()
-            if default_msg:
-                reply = default_msg.reply(message)
-                if reply:
-                    return reply
-
-            current_app.logger.error('test4')
-            return TransferCustomerServiceReply(message=message)
+                follow_msg = Message.objects(follow=True).first()
+                if follow_msg:
+                    reply = follow_msg.reply(message)
+                    if reply:
+                        return reply
+                default_msg = Message.objects(default=True).first()
+                if default_msg:
+                    reply = default_msg.reply(message)
+                    if reply:
+                        return reply
+                return TransferCustomerServiceReply(message=message)
+            except:
+                current_app.logger.error(traceback.format_ext())
+                return '系统繁忙，请稍后重试！'
 
         @robot.unsubscribe
         def on_unsubscribe(message):
