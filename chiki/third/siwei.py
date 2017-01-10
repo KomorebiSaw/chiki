a95ea3e1d225639e19628d96b25cc925860f3575
tiger <438985635@qq.com>
2017-01-11 00:16:36 +0800

    update

--- chiki/third/siwei.py
+++ chiki/third/siwei.py
@@ -57,7 +57,7 @@ class SiWei(object):
                         tpl % (sign, curr_sign, request.form))
                     return '{"message":"签名错误","response":"-1"}'
                 if self.callback:
-                    res = self.callback(self.decode(data, self.token.get('secretkey')))
+                    res = self.callback(self.decode(data, self.config.get('aes_key')))
             except:
                 current_app.logger.error(
                     'siwei callbck except: \n%s' % traceback.format_exc())
