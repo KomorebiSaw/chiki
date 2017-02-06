55d74a69cce14b40340c360dd226ed1f78f63cb2
tiger <438985635@qq.com>
2017-02-06 15:43:41 +0800

    update

--- chiki/patch.py
+++ chiki/patch.py
@@ -14,14 +14,15 @@ funcs = dict()
 
 def url_for(endpoint, **values):
     if current_app.config.get('PATCH_URL'):
-        arg1 = ''.join(random.sample(case, random.randint(2, 6)))
-        return old_url_for(endpoint, _arg1=arg1, **values)
+        arg1 = ''.join(random.sample(case, random.randint(2, 4)))
+        arg2 = ''.join(random.sample(case, random.randint(2, 4)))
+        return old_url_for(endpoint, _arg1=arg1, _arg2=arg2, **values)
     return old_url_for(endpoint, **values)
 
 
 @setupmethod
 def add_url_rule(self, rule, endpoint=None, view_func=None, **options):
-    rule2 = '/<_arg1>-1/' + rule[1:]
+    rule2 = '/<_arg1>-1/' + rule[1:] + '/<_arg2>.html'
 
     func = view_func
     if self.config.get('PATCH_URL') and func:
@@ -31,12 +32,14 @@ def add_url_rule(self, rule, endpoint=None, view_func=None, **options):
             def func(*args, **kwargs):
                 if '_arg1' in kwargs:
                     kwargs.pop('_arg1')
+                if '_arg2' in kwargs:
+                    kwargs.pop('_arg2')
                 return view_func(*args, **kwargs)
         funcs[view_func] = func
 
     old_add_url_rule(self, rule, endpoint=endpoint,
                      view_func=func, **options)
-    options.setdefault('defaults', {'_arg1': ''})
+    options.setdefault('defaults', {'_arg1': '', '_arg2': ''})
     return old_add_url_rule(self, rule2, endpoint=endpoint,
                             view_func=func, **options)
 
