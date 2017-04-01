3d4ac0b74fe5517ee1a94ea98a28b6e1ac2454e2
tiger <438985635@qq.com>
2017-04-01 18:01:17 +0800

    update

--- chiki/third/near.py
+++ chiki/third/near.py
@@ -19,14 +19,16 @@ def near_required(func):
     @login_required
     def wrapper(*args, **kwargs):
         last = Item.time('near_openid_time', '', name='Near刷新时间')
-        if not current_user.near_openid or current_user.neared < last:
-            ret = url_for('near_openid_callback',
-                          next=request.url, _external=True)
-            query = urlencode(dict(redUrl=ret))
-            url = 'http://qqzz.ps798.cn/wxOpenId/getOpenId?%s' % query
-            if current_app.debug:
-                current_app.logger.info(url)
-            return redirect(url)
+        if 'near' in Item.data('payment', ''):
+            host = Item.data('near_host', 'qqzz.ps798.cn', name='Near域名')
+            if not current_user.near_openid or current_user.neared < last:
+                ret = url_for('near_openid_callback',
+                              next=request.url, _external=True)
+                query = urlencode(dict(redUrl=ret))
+                url = 'http://%s/wxOpenId/getOpenId?%s' % (host, query)
+                if current_app.debug:
+                    current_app.logger.info(url)
+                return redirect(url)
         return func(*args, **kwargs)
     return wrapper
 
