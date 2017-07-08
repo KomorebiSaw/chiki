b73b8777ba45ff74cc9433af7def22d4565cb2f0
tiger <438985635@qq.com>
2017-07-08 22:52:38 +0800

    auto commit

--- chiki/contrib/users/models.py
+++ chiki/contrib/users/models.py
@@ -509,6 +509,7 @@ class UserLog(db.Document):
         (TYPE_BIND, '绑定手机'),
         (TYPE_REGISTER, '注册'),
         (TYPE_LOGIN, '登录'),
+        (TYPE_LOGIN_ERROR, '登录错误'),
         (TYPE_LOGOUT, '退出'),
         (TYPE_CHANGE_PASSWORD, '修改密码'),
         (TYPE_RESET_PASSWORD, '重置密码'),
@@ -539,39 +540,42 @@ class UserLog(db.Document):
     def log(type, id, device, key='', spm=None, ip=None):
         spm = spm if spm else get_spm()
         ip = ip if ip else get_ip()
-        UserLog(user=id, type=type, device=device, key=key, spm=spm, ip=ip).save()
+        um.models.UserLog(user=id, type=type, device=device,
+                          key=key, spm=spm, ip=ip).save()
 
     @staticmethod
     def active(id, device='', key='', spm=None, ip=None):
-        UserLog.log(UserLog.TYPE_ACTIVE, id, device, key, spm, ip)
+        um.models.UserLog.log(UserLog.TYPE_ACTIVE, id, device, key, spm, ip)
 
     @staticmethod
     def bind(id, device, key='', spm=None, ip=None):
-        UserLog.log(UserLog.TYPE_BIND, id, device, key, spm, ip)
+        um.models.UserLog.log(UserLog.TYPE_BIND, id, device, key, spm, ip)
 
     @staticmethod
     def register(id, device, key='', spm=None, ip=None):
-        UserLog.log(UserLog.TYPE_REGISTER, id, device, key, spm, ip)
+        um.models.UserLog.log(UserLog.TYPE_REGISTER, id, device, key, spm, ip)
 
     @staticmethod
     def login(id, device, key='', spm=None, ip=None):
-        UserLog.log(UserLog.TYPE_LOGIN, id, device, key, spm, ip)
+        um.models.UserLog.log(UserLog.TYPE_LOGIN, id, device, key, spm, ip)
 
     @staticmethod
     def login_error(id, device, key='', spm=None, ip=None):
-        UserLog.log(UserLog.TYPE_LOGIN_ERROR, id, device, key, spm, ip)
+        um.models.UserLog.log(UserLog.TYPE_LOGIN_ERROR,
+                              id, device, key, spm, ip)
 
     @staticmethod
     def logout(id, device, key='', spm=None, ip=None):
-        UserLog.log(UserLog.TYPE_LOGOUT, id, device, key, spm, ip)
+        um.models.UserLog.log(UserLog.TYPE_LOGOUT, id, device, key, spm, ip)
 
     @staticmethod
     def change_password(id, device, key='', spm=None, ip=None):
-        UserLog.log(UserLog.TYPE_CHNAGE_PASSWORD, id, device, key, spm, ip)
+        um.models.UserLog.log(UserLog.TYPE_CHNAGE_PASSWORD,
+                              id, device, key, spm, ip)
 
     @staticmethod
     def reset_password(id, device, key='', spm=None, ip=None):
-        UserLog.log(UserLog.TYPE_RESET_PASSWORD, id, device, key, spm, ip)
+        um.models.UserLog.log(UserLog.TYPE_RESET_PASSWORD, id, device, key, spm, ip)
 
 
 class PhoneCode(db.Document):
