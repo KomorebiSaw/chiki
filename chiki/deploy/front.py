b99f559b62561a921394f9d2d9b46ec9b5e964ee
tiger <438985635@qq.com>
2017-07-11 15:42:03 +0800

    auto commit

--- chiki/deploy/front.py
+++ chiki/deploy/front.py
@@ -1,20 +1,60 @@
 # coding: utf-8
-from fabric.api import env, local, task, roles, run
+from fabric.api import env, local, task, roles, run, runs_once, settings
+from datetime import datetime
+from .web import restart as _restart, restart_back
+from .utils import execute, xput
+
+
+@task
+@roles('web')
+def upload_etc(app='web'):
+    config = '%s/etc/config' % env.path
+    xput('etc/%s.py' % app, config)
 
 
 @task
 @roles('media')
-def media(source='media/web/dist', target=None):
-    target = target or env.project
-    run('mkdir -p /var/htdocs/www/%s' % target)
+@runs_once
+def dist_media(source='../media/web/dist', target=None):
+    now = datetime.now().strftime('%Y%m%d%H%M')
+    target = target or '%s%s' % (env.project, now)
+
+    local('cp -r %s /tmp/%s' % (source, target))
+    run('mkdir -p /var/htdocs/www')
     local(r"""
 
 expect -c "
-spawn scp -r %s/* %s@%s:/var/htdocs/www/%s
+spawn scp -r /tmp/%s %s@%s:/var/htdocs/www
 expect {
     \"*assword\" {set timeout 500; send \"%s\r\";}
     \"yes/no\" {send \"yes\r\"; exp_continue;}
 }
 expect eof"
 
-        """ % (source, env.user, env.host, target, env.password))
+        """ % (target, env.user, env.host, env.password))
+
+
+@task
+@runs_once
+def media(source='../media/web/dist', target=None, app='web', restart=True):
+    restart = True if restart in ['True', 'true', True] else False
+    now = datetime.now().strftime('%Y%m%d%H%M')
+    target = target or '%s%s' % (env.project, now)
+
+    execute(dist_media)
+
+    for stage, e in env.envs.iteritems():
+        if stage not in env.exclude_envs:
+            tpl = (
+                r'sed -r -i "s#SITE_STATIC_PREFIX.*#SITE_STATIC_PREFI'
+                r'X = \"http://cdn.51cxin.cn/%s/\"#" %s/etc/%s.py'
+            )
+            local(tpl % (target, stage, app))
+
+    with settings(stage='all'):
+        execute(upload_etc, app)
+
+    if restart:
+        with settings(stage='all'):
+            execute(_restart)
+            execute(restart_back)
